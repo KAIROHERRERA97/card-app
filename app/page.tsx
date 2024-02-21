185c0c174e6145728c9f8bb3f3e8158ec00d4ec2
KAIROHERRERA97 <136655999+KAIROHERRERA97@users.noreply.github.com>
2024-02-21 13:00:45 -0500

    fix: import of cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
-import Image from "next/image";
-import Card01 from "./ui/cards/cards_01";
-import Card02 from "./ui/cards/cards_02";
-import Card03 from "./ui/cards/cards_03";
-import Card04 from "./ui/cards/cards_04";
-import Card05 from "./ui/cards/cards_05";
-import Card06 from "./ui/cards/cards_06";
+// import Image from "next/image";
+import Card01 from "./ui/card-01";
+import Card02 from "./ui/card-02";
+import Card03 from "./ui/card-03";
+import Card04 from "./ui/card-04";
+
+
 
 export default function Home() {
   return (
     <div className="flex w-full px-20 py-10 flex-col ">
       <h1 className="text-2xl font-semibold  text-center   mb-10  text-gray-700 ">
-        My Cards, Fernanda
+        My Cards, Antony Herrera
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-8 ">
         <Card01></Card01>
         <Card02></Card02>
         <Card03></Card03>
         <Card04></Card04>
-        <Card05></Card05>
-        <Card06></Card06>
       </div>
     </div>
   );
